dc1cd21e60ab2d233c63444d114e7bf223cb131f
Alex Mekic <59836295+kuroyoshi10@users.noreply.github.com>
2020-08-18 13:40:08 -0400

    Added download and install for PXE Management Application

--- Clonezilla Install Script File/ClonezillaInstall.py
+++ Clonezilla Install Script File/ClonezillaInstall.py
@@ -1,50 +1,59 @@
-import os, sys, wget, requests, shutil
+import os, sys, wget, requests, shutil, json
 from bs4 import BeautifulSoup
 from zipfile import ZipFile
 
-def check_clonezilla_update(zfs_pool):
-    print("Checking for latest version of Clonezilla...")
-    website_url = requests.get('https://clonezilla.org/downloads.php')
-    soup = BeautifulSoup(website_url.content,'html5lib')
-    latest_version = soup.find('a', attrs = {'href':'./downloads/download.php?branch=stable'}).find('font', attrs = {'color':'red'}).text
-    if update_clonezilla(latest_version, zfs_pool):
-        print("Clonezilla version " + latest_version + " installed successfully")
-    else:
-        print("Clonezilla install failed")
-
-def update_clonezilla(latest_version, zfs_pool):
-    print("Downloading Clonezilla " + latest_version + "...")
-    url1 = 'http://free.nchc.org.tw/clonezilla-live/stable/clonezilla-live-' + str(latest_version) + '-amd64.zip'
+def download_management(zfs_pool):
+    print("Checking for PXE Management Application...", end='', flush=True)
     try:
-        with open('/' + zfs_pool + '/tftp/clonezilla_download.zip', 'wb') as f:
-            response = requests.get(url1, stream=True)
-            total = response.headers.get('content-length')
-            if total is None:
-                f.write(response.content)
-            else:
-                downloaded = 0
-                total = int(total)
-                for data in response.iter_content(chunk_size=128):
-                    downloaded += len(data)
-                    f.write(data)
-                    done = int(50*downloaded/total)
-                    sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
-                    sys.stdout.flush()
-        sys.stdout.write('\n')
-        #wget.download(url=url1, out='/' + zfs_pool + '/tftp/clonezilla_update.zip')
-        print('done')
+        website_url = 'https://api.github.com/repos/kuroyoshi10/clonezillaserver-management/releases'
+        latest_version = json.loads(requests.get(website_url).text)[0]['tag_name']
+        print("done")
+        try:
+            print("Downloading and Installing PXE Management Application...", end='', flush=True)
+            url_download = "https://github.com/kuroyoshi10/clonezillaserver-management/releases/download/" + latest_version + "/pxe_management"
+            wget.download(url=url_download, out='/' + zfs_pool + '/pxe_management/pxe_management')
+            print("done")
+            print("PXE Management Application installed successfully")
+        except:
+            print("failed")
     except:
-        print("unable to download Clonezilla update. Check Internet connection and run command ./ClonezillaInstall after reboot.")
+        print("failed")
+
+def check_clonezilla(zfs_pool):
+    print("Checking for latest version of Clonezilla...", end='', flush=True)
+    try:
+        website_url = requests.get('https://clonezilla.org/downloads.php')
+        soup = BeautifulSoup(website_url.content,'html5lib')
+        latest_version = soup.find('a', attrs = {'href':'./downloads/download.php?branch=stable'}).find('font', attrs = {'color':'red'}).text
+        print("done")
+        if install_clonezilla(latest_version, zfs_pool):
+            print("Clonezilla " + latest_version + " installed successfully")
+        else:
+            print("Clonezilla install failed")
+    except:
+        print("failed")
+
+def install_clonezilla(latest_version, zfs_pool):
+    print("Downloading Clonezilla...")
+    url_download = 'https://sourceforge.net/projects/clonezilla/files/clonezilla_live_stable/' + str(latest_version) + '/clonezilla-live-' + str(latest_version) + '-amd64.zip/download'
+    try:
+        wget.download(url=url_download, out='/' + zfs_pool + '/tftp/clonezilla_update.zip')
+        print('done')
+        print('Installing Clonezilla...', end='', flush=True)
+        shutil.rmtree('/' + zfs_pool + '/tftp/clonezilla')  
+        os.mkdir('/' + zfs_pool + '/tftp/clonezilla')
+        with ZipFile('/' + zfs_pool + '/tftp/clonezilla_update.zip', 'r') as zipObj:
+            zipObj.extractall('/' + zfs_pool + '/tftp/clonezilla')
+        print('done')
+        print('Cleaning up download file...', end='', flush=True)
+        os.remove('/' + zfs_pool + '/tftp/clonezilla_update.zip')
+        print('done')
+        return True
+    except:
+        print("failed")
         return False
-    print("Extracting Clonezilla " + latest_version + "...", end='', flush=True)
-    with ZipFile('/' + zfs_pool + '/tftp/clonezilla_download.zip', 'r') as zipObj:
-        zipObj.extractall('/' + zfs_pool + '/tftp/clonezilla')
-    print('done')
-    print('Cleaning up...', end='', flush=True)
-    os.remove('/' + zfs_pool + '/tftp/clonezilla_download.zip')
-    print('done')
-    return True
 
 if __name__ == "__main__":
     zfs_pool = sys.argv[1]
-    check_clonezilla_update(zfs_pool)
+    download_management(zfs_pool)
+    check_clonezilla(zfs_pool)
